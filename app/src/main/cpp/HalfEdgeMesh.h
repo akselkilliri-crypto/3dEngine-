fc4dc3adc15fb706fa1e54e535b6c56b7ac68d8b
akselkilliri-crypto <akselkilliri@gmail.com>
2026-04-15 23:04:48 +0300

    Create HalfEdgeMesh.h

---
+++ app/src/main/cpp/HalfEdgeMesh.h
@@ -0,0 +1,49 @@
+#ifndef HALFEDGEMESH_H
+#define HALFEDGEMESH_H
+
+#include <vector>
+#include <GLES3/gl3.h>
+#include "Math.h"
+
+struct VertexHE {
+    Vec3 position;
+    Vec3 normal;
+    float color[4];
+    int id;
+};
+
+struct Face {
+    std::vector<int> vertexIndices; // индексы вершин в массиве
+    Vec3 normal;
+    bool selected;
+    Face() : selected(false) {}
+};
+
+class HalfEdgeMesh {
+public:
+    HalfEdgeMesh();
+    ~HalfEdgeMesh();
+
+    void createCube();
+    void drawFaces(GLuint program, GLenum mode);
+    void drawSelectedFaces(GLuint program, GLenum mode);
+
+    // Ray casting: возвращает индекс грани, в которую попал луч из точки касания
+    int pickFace(const Mat4& mvp, const Mat4& view, int screenWidth, int screenHeight, float touchX, float touchY);
+
+    void toggleFaceSelection(int faceIndex);
+
+private:
+    std::vector<VertexHE> vertices;
+    std::vector<unsigned int> faceIndices;      // индексы для всех граней (треугольники)
+    std::vector<unsigned int> selectedIndices;  // индексы для выделенных граней
+    std::vector<Face> faces;
+
+    GLuint VAO, VBO, EBO;
+    bool dirty;
+
+    void updateGPUBuffers();
+    void updateSelectedBuffers();
+};
+
+#endif
