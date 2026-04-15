7964bd3db3c34ba37508dcb8230d22b2c9e742f4
akselkilliri-crypto <akselkilliri@gmail.com>
2026-04-15 23:27:48 +0300

    Update Math.h

--- app/src/main/cpp/Math.h
+++ app/src/main/cpp/Math.h
@@ -9,6 +9,8 @@ struct Vec3 {
     Vec3() : x(0), y(0), z(0) {}
     Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
     Vec3 operator-(const Vec3& other) const { return Vec3(x - other.x, y - other.y, z - other.z); }
+    Vec3 operator+(const Vec3& other) const { return Vec3(x + other.x, y + other.y, z + other.z); }
+    Vec3 operator*(float s) const { return Vec3(x * s, y * s, z * s); }
     Vec3 cross(const Vec3& other) const {
         return Vec3(y*other.z - z*other.y, z*other.x - x*other.z, x*other.y - y*other.x);
     }
@@ -24,6 +26,7 @@ struct Vec4 {
     float x, y, z, w;
     Vec4() : x(0), y(0), z(0), w(1) {}
     Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+    Vec4 operator*(float s) const { return Vec4(x*s, y*s, z*s, w*s); }
 };
 
 struct Mat4 {
