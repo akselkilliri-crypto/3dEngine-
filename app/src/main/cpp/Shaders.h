c3fee1253f9565737c0a9c498406c31ad3eff9b5
akselkilliri-crypto <akselkilliri@gmail.com>
2026-04-15 14:57:17 +0300

    Update Shaders.h

--- app/src/main/cpp/Shaders.h
+++ app/src/main/cpp/Shaders.h
@@ -4,18 +4,15 @@
 static const char* vertexShaderSource = R"(
 #version 300 es
 uniform mat4 u_MVPMatrix;
-uniform mat4 u_MVMatrix;
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec3 a_Normal;
 layout(location = 2) in vec4 a_Color;
 
-out vec3 v_Normal;
 out vec4 v_Color;
 
 void main() {
     gl_Position = u_MVPMatrix * vec4(a_Position, 1.0);
-    v_Normal = mat3(u_MVMatrix) * a_Normal;
     v_Color = a_Color;
 }
 )";
@@ -24,18 +21,11 @@ static const char* fragmentShaderSource = R"(
 #version 300 es
 precision highp float;
 
-in vec3 v_Normal;
 in vec4 v_Color;
-
-uniform sampler2D u_MatCap;
-
 out vec4 fragColor;
 
 void main() {
-    vec3 normal = normalize(v_Normal);
-    vec2 matcapCoord = normal.xy * 0.5 + 0.5;
-    vec4 matcapColor = texture(u_MatCap, matcapCoord);
-    fragColor = matcapColor * v_Color;
+    fragColor = v_Color;
 }
 )";
 
