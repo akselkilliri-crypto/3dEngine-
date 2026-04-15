e00f47b3f8fd1f2248f231c9036a5fde5944219a
akselkilliri-crypto <akselkilliri@gmail.com>
2026-04-15 16:28:30 +0300

    Update Mesh.h

--- app/src/main/cpp/Mesh.h
+++ app/src/main/cpp/Mesh.h
@@ -8,14 +8,14 @@
 struct Vertex {
     Vec3 position;
     Vec3 normal;
-    float color[4]; // RGBA
+    float color[4];
 };
 
 class Mesh {
 public:
     Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices);
     ~Mesh();
-    void draw(GLuint program);
+    void draw(GLuint program, GLenum mode);
 
 private:
     GLuint VAO, VBO, EBO;
