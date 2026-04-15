16079c2a886070af48d2bf9adbd9d0c3089ca657
akselkilliri-crypto <akselkilliri@gmail.com>
2026-04-16 00:25:18 +0300

    Update MyGLRenderer.kt

--- app/src/main/java/com/example/modelinengine/MyGLRenderer.kt
+++ app/src/main/java/com/example/modelinengine/MyGLRenderer.kt
@@ -19,6 +19,7 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
     external fun nativeRender()
     external fun nativeOnTouchEvent(action: Int, x: Float, y: Float)
     external fun nativeOnScale(scaleFactor: Float)
+    external fun nativeSubdivide()
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         nativeInit()
